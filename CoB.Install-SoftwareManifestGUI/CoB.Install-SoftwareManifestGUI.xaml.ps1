6348507e0bfb4685b73aa48026ba4096642ad8d8
Drew Westgate <drewsy1@gmail.com>
2018-01-31 15:35:05 -0600

    Renamed files

--- CoB.Install-SoftwareManifestGUI/CoB.Install-SoftwareManifestGUI.xaml.ps1
+++
@@ -1,9 +0,0 @@
-﻿[xml]$xaml = Get-Content -Path "$PSScriptRoot\CoB.Install-SoftwareManifestGUI.xaml"
-$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
-$window = [Windows.Markup.XamlReader]::Load($xamlReader)
-
-$xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]")  | ForEach-Object { 
-    New-Variable -Name $_.Name -Value $window.FindName($_.Name) -Force
-}
-
-$window.ShowDialog() | Out-Null
---
+++ CoB.Install-SoftwareManifestGUI/MainWindow.xaml.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module CoB.SoftwareManagement
+
+[xml]$xaml = Get-Content -Path "$PSScriptRoot\MainWindow.xaml"
+$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
+$window = [Windows.Markup.XamlReader]::Load($xamlReader)
+
+$xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]")  | ForEach-Object { 
+    New-Variable -Name $_.Name -Value $window.FindName($_.Name) -Force
+}
+
+function Set-DataGridContents
+{
+
+}
+$array = New-Object System.Collections.ArrayList
+$Script:procInfo = CoB.SoftwareManagement\Get-CoBPak | Select Company,Name,Version,Architecture | sort -Property Company
+$array.AddRange($procInfo)
+$listCatalog.ItemsSource = $array
+
+$window.ShowDialog()
