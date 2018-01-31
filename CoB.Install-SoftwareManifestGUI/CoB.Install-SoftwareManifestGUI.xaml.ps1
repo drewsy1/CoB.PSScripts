8c2d1bdc37e2e50f978769ea70db0379a952806b
Drew Westgate <drewsy1@gmail.com>
2018-01-30 19:55:17 -0600

    Initial commit.

---
+++ CoB.Install-SoftwareManifestGUI/CoB.Install-SoftwareManifestGUI.xaml.ps1
@@ -0,0 +1,9 @@
+﻿[xml]$xaml = Get-Content -Path "$PSScriptRoot\CoB.Install-SoftwareManifestGUI.xaml"
+$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
+$window = [Windows.Markup.XamlReader]::Load($xamlReader)
+
+$xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]")  | ForEach-Object { 
+    New-Variable -Name $_.Name -Value $window.FindName($_.Name) -Force
+}
+
+$window.ShowDialog() | Out-Null
